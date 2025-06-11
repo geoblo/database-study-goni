c517842613f706c5ff9af6ec6c43b8ea77f8c7b4
geoblo <geoblo.naver.com>
2025-06-11 10:18:52 +0900

    up

--- source/ch02_selfcheck.sql
+++ source/ch02_selfcheck.sql
@@ -24,3 +24,48 @@
 -- 6 콜드브루를 삭제하세요.
 -- 7 모든 커피 데이터를 조회하세요.
 
+-- 1. starbuuks DB 생성 및 진입
+CREATE DATABASE starbuuks;
+USE starbuuks;
+
+-- 확인: 현재 사용 중인 DB 조회
+SELECT DATABASE();
+
+-- 2. coffees 테이블 생성
+CREATE TABLE coffees (
+	id INTEGER, -- 정수형
+    -- id INTEGER PRIMARY KEY, -- 또는 컬럼을 정의할 때 기본키 지정 가능
+    name VARCHAR(20), -- 문자형(최대 20글자)
+    price INTEGER, -- 정수형(원)
+    PRIMARY KEY (id) -- 기본키: id
+);
+
+-- 확인: 테이블 구조 조회
+DESC coffees;
+
+-- 3. coffees 데이터 삽입
+INSERT INTO coffees (id, name, price)
+VALUES
+	(1, '아메리카노', 3800),
+	(2, '카페라떼', 4000),
+	(3, '콜드브루', 3500),
+	(4, '카페모카', 4500),
+	(5, '카푸치노', 5000);
+
+-- 4. 모든 커피 이름 조회
+SELECT name
+FROM coffees;
+
+-- 5. 카푸치노 가격 200원 인상(연산식 사용 가능)
+UPDATE coffees
+SET price = price + 200 -- 기존 가격 + 200
+WHERE id = 5;
+
+-- 6. 콜드브루 삭제
+DELETE FROM coffees
+WHERE id = 3;
+
+-- 7. 모든 커피 데이터 조회
+SELECT *
+FROM coffees;
+
