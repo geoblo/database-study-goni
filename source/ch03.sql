b82452adb98f6b720b6c36a8ef88fb7b26e3b0ff
geoblo <geoblo.naver.com>
2025-06-11 11:45:27 +0900

    sd

--- source/ch03.sql
+++ source/ch03.sql
@@ -47,8 +47,58 @@ VALUES
 	(3, '맨스파이시 상해 버거', 5300, 235, 494, 20),
 	(4, '슈비두밥 버거', 6200, 269, 563, 21),
 	(5, '더블 쿼터파운드 치즈', 7700, 275, 770, 50);
+    
+-- 모든 버거 조회
+SELECT * 
+FROM burgers;
 
+-- 1) 비교 연산자
+-- 두 값을 비교하는 연산 기호
+-- WHERE 절에 사용하여 특정 데이터로 필터링 가능
+-- 종류: =, !=, >, >=, <, <=
 
+-- 가격이 5500원 보다 싼 버거 찾기
+SELECT *
+FROM burgers
+WHERE price < 5500;
+
+-- 가격이 5500원 보다 비싼 버거 찾기
+SELECT *
+FROM burgers
+WHERE price > 5500;
+
+-- 단백질량이 25g 보다 적은 버거 찾기
+SELECT *
+FROM burgers
+WHERE protein < 25;
+
+-- 2) 논리 연산자
+-- 조건을 조합하거나 반전하여 새 조건을 만듦
+-- 종류: AND, OR, NOT
+-- 사용 예
+-- 조건A AND 조건B: 조건A와 조건B를 동시에 만족하는 데이터 필터링(교집합)
+-- 조건A OR 조건B: 조건A와 조건B 중 하나라도 만족하는 데이터 필터링(합집합)
+-- NOT 조건A: 조건A를 만족하지 않는 데이터 필터링(여집합)
+
+-- TRUE/FALSE의 논리 연산
+-- TRUE(참: 1), FALSE(거짓: 0)
+-- AND 연산: 둘 다 참이여야 참
+SELECT TRUE AND TRUE; -- 1
+SELECT TRUE AND FALSE; -- 0
+SELECT FALSE AND TRUE; -- 0
+SELECT FALSE AND FALSE; -- 0
+
+-- OR 연산: 둘 중 하나만 참이면 참
+SELECT TRUE OR TRUE; -- 1
+SELECT TRUE OR FALSE; -- 1
+SELECT FALSE OR TRUE; -- 1
+SELECT FALSE OR FALSE; -- 0
+
+-- 5500원 보다 싸고, 동시에 단백질량이 25g 보다 많은 버거
+
+-- 5500원 보다 싸거나, 단백질량이 25g 보다 많은 버거
+
+-- 단백질량이 25g 보다 많지 않은 버거
 
 
 
