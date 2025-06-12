317221e0ba38f4959a7f937dfc81b33ca0ed8665
geoblo <geoblo.naver.com>
2025-06-12 09:49:15 +0900

    sd

--- source/ch04_selfcheck.sql
+++ source/ch04_selfcheck.sql
@@ -29,7 +29,22 @@
 -- 6 Sales 부서의 최대 연봉과 최소 연봉 차이를 구하세요.
 -- 7 가장 높은 연봉을 받는 직원은 전체 평균 연봉 대비 얼마를 더 받는지 구하세요.
 
+-- 1. company DB 생성 및 진입, employees 테이블 생성
+CREATE DATABASE company;
+USE company;
 
+CREATE TABLE employees (
+	id INTEGER,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INTEGER,
+    PRIMARY KEY (id)
+);
+
+-- 2. 직원 10명의 데이터 삽입
+INSERT INTO 
+	employees (id, name, department, salary)
+VALUES
 	(101, 'John', 'Sales', 7000),
 	(102, 'Aria', 'IT', 5500),
 	(103, 'Mike', 'Sales', 8000),
@@ -40,5 +55,29 @@
 	(108, 'Sophia', 'HR', 6300),
 	(109, 'Lucas', 'Sales', 5500),
 	(110, 'Charlotte', 'HR', 6800);
+    
+-- 확인
+SELECT * FROM employees;
 
+-- 3. 모든 직원의 연봉 합계
+SELECT SUM(salary)
+FROM employees;
+
+-- 4. Sales 부서의 평균 연봉
+SELECT AVG(salary)
+FROM employees
+WHERE department = 'Sales';
+
+-- 5. 모든 부서 수 조회
+SELECT COUNT(DISTINCT department)
+FROM employees;
+
+-- 6. Sales 부서의 최대 연봉과 최소 연봉 차이
+SELECT MAX(salary) - MIN(salary)
+FROM employees
+WHERE department = 'Sales';
+
+-- 7. 최대 연봉 수령자의 연봉과 평균 연봉과의 차이
+SELECT MAX(salary) - AVG(salary)
+FROM employees;
 
